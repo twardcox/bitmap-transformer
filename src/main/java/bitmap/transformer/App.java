bbfc8334476eb8e2153644f7593cfe5395797a99
twardcox <t.ward.cox@gmail.com>
2019-08-09 13:39:39 -0700

    Completed Tests

--- src/main/java/bitmap/transformer/App.java
+++ src/main/java/bitmap/transformer/App.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class App {
   public static void main(String[] args) throws IOException {
-    Bitmap coffee = new Bitmap(args[0], args[1], args[2]);
+      Bitmap coffee = new Bitmap(args[0], args[1], args[2]);
 
   }
 }
--- src/test/java/bitmap/transformer/BitmapTest.java
+++ src/test/java/bitmap/transformer/BitmapTest.java
@@ -4,11 +4,25 @@
 package bitmap.transformer;
 
 import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.*;
 
-public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+public class BitmapTest {
+    @Test public void testConvertBW() {
+        Bitmap classUnderTest = new Bitmap("Coffee", "convertBW", "convertBW");
+        assertTrue("app should create a file named CopyCoffee in the main resources file.", Files.exists(Paths.get("./src/main/resources/convertBW.bmp")));
+    }
+
+    @Test public void testFlipImage() {
+        Bitmap classUnderTest = new Bitmap("Coffee", "flipImage", "flipImage");
+        assertTrue("app should create a file named CopyCoffee in the main resources file.", Files.exists(Paths.get("./src/main/resources/flipImage.bmp")));
+    }
+
+    @Test public void testHalfSizeIt() {
+        Bitmap classUnderTest = new Bitmap("Coffee", "halfSizeIt", "halfSizeIt");
+        assertTrue("app should create a file named CopyCoffee in the main resources file.", Files.exists(Paths.get("./src/main/resources/halfSizeIt.bmp")));
     }
 }
